1d9cbaec6d3ef427560b00800f3ecb366bd13945
jgm0 <jacobgermanamccray@gmail.com>
2024-07-06 15:14:40 -0400

    figuring out token structure

---
+++ src/lexer/lexer.rs
@@ -0,0 +1,1 @@
+use super::token::{Token, TokenType, Value, Position, Operator, Keyword};
---
+++ src/lexer/token.rs
@@ -0,0 +1,139 @@
+use std::fmt;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Position {
+    pub row: usize,
+    pub col: usize,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Operator {
+    // Arithmetic
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Modulo,
+    Increment,
+    Decrement,
+
+    // Bitwise
+    BitwiseAnd,
+    BitwiseOr,
+    BitwiseXor,
+    BitwiseNot,
+    LeftShift,
+    RightShift,
+
+    // Logical
+    LogicalAnd,
+    LogicalOr,
+    LogicalNot,
+
+    // Comparison
+    Equal,
+    NotEqual,
+    LessThan,
+    GreaterThan,
+    LessThanOrEqual,
+    GreaterThanOrEqual,
+
+    // Assignment
+    Assign,
+    AddAssign,
+    SubtractAssign,
+    MultiplyAssign,
+    DivideAssign,
+    ModuloAssign,
+    BitwiseAndAssign,
+    BitwiseOrAssign,
+    BitwiseXorAssign,
+    LeftShiftAssign,
+    RightShiftAssign,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Keyword {
+    Auto,
+    Break,
+    Case,
+    Char,
+    Const,
+    Continue,
+    Default,
+    Do,
+    Double,
+    Else,
+    Enum,
+    Extern,
+    Float,
+    For,
+    Goto,
+    If,
+    Int,
+    Long,
+    Register,
+    Return,
+    Short,
+    Signed,
+    Sizeof,
+    Static,
+    Struct,
+    Switch,
+    Typedef,
+    Union,
+    Unsigned,
+    Void,
+    Volatile,
+    While,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenType {
+    Identifier,
+    Keyword(Keyword),
+    Operator(Operator),
+    IntegerLiteral,
+    FloatLiteral,
+    CharLiteral,
+    StringLiteral,
+    Punctuator(char),
+    Comment,
+    Whitespace,
+    EOF,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Value {
+    String(String),
+    Integer(i64),
+    Float(f64),
+    Char(char),
+}
+
+#[derive(Debug, Clone)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub value: Value,
+    pub position: Position,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Token {{ type: {:?}, value: {:?}, position: ({}, {}) }}",
+            self.token_type, self.value, self.position.row, self.position.col
+        )
+    }
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, value: Value, row: usize, col: usize) -> Self {
+        Token {
+            token_type,
+            value,
+            position: Position { row, col },
+        }
+    }
+}
